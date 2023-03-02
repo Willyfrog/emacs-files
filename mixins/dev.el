130049acc8207e5818c6781e754c7688844bd8f8
= <=>
2023-03-02 22:49:22 +0100

    initial commit bedrock+lisp+go

---
+++ mixins/dev.el
@@ -0,0 +1,83 @@
+;;; Emacs Bedrock
+;;;
+;;; Mixin: Development tools
+
+;;; Usage: Append or require this file from init.el for some software
+;;; development-focused packages.
+;;;
+;;; It is **STRONGLY** recommended that you use the ui.el mixin if you want to
+;;; use eglot. Lots of completion things will work better.
+
+;;; Contents:
+;;;
+;;;  - Version Control
+;;;  - Common file types
+;;;  - Eglot, the built-in LSP client for Emacs
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Version Control
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Magit: best Git client to ever exist
+(use-package magit
+  :ensure t
+  :bind (("s-g" . magit-status)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Common file types
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package markdown-mode
+  :ensure t)
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package json-mode
+  :ensure t)
+
+;; You're almost certain to find a mode for the language you're looking for.
+;; Search the internet to find the right one.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Eglot, the built-in LSP client for Emacs
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package eglot
+  ;; no :ensure t here because it's built-in
+
+  ;; Configure hooks to automatically turn-on eglot for selected modes
+  ; :hook
+  ; ((python-mode . eglot))
+
+  :custom
+  (eglot-send-changes-idle-time 0.1)
+
+  :config
+  ;; Sometimes you need to tell Eglot where to find the language server
+  ; (add-to-list 'eglot-server-programs
+  ;              '(haskell-mode . ("haskell-language-server-wrapper" "--lsp")))
+  )
+
+;; SLIME
+(load (expand-file-name "~/.quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "sbcl")
+
+;; company mode
+(use-package company
+  :ensure t)
+
+(use-package go-mode
+  :ensure t
+  :mode "\\.go\\'")
+
+(use-package gotest
+  :ensure t
+  :bind (("C-x f" . go-test-current-file)
+         ("C-x t" . go-test-current-test)))
